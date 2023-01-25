7342aeefee1e7f6dd322beb5c05b376b863919da
Vladislav <vpfedotoff@gmail.com>
2023-01-25 19:47:37 +0300

    added Task043

---
+++ Task043/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 43: Написать программу, которая на вход принимает массив из любого количества элементов (не менее 6)в промежутке от 0 до 100, а на выходе выводит этот же массив, но отсортированный по возрастанию(от меньшего числа к большему).
+
+int length = new Random().Next(6, 100);
+int[] numbers = new int[length];
+
+int[] FillArray(int[] array)
+{
+    for(int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(0, 101);
+    }
+    return array;
+}
+void Sort(int[] array)
+{
+int temp;
+for (int i = 0; i < array.Length - 1; i++)
+{
+    for (int j = i + 1; j < array.Length; j++)
+    {
+        if (array[i] > array[j])
+        {
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+}
+
+FillArray(numbers);
+Sort(numbers);
